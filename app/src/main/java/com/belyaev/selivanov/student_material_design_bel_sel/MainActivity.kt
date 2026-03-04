1b6187a4434f2023b4766c0861939d714f27014e
s0m1k111 <spavel1207@gmail.com>
2026-03-04 15:27:34 +0300

    Step 6: Custom color scheme added

--- app/src/main/java/com/belyaev/selivanov/student_material_design_bel_sel/MainActivity.kt
+++ app/src/main/java/com/belyaev/selivanov/student_material_design_bel_sel/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.res.painterResource
@@ -43,7 +44,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun StudentIcon(
     @DrawableRes studentIcon: Int,
@@ -80,24 +80,34 @@ fun StudentInformation(
 fun StudentItem(
     student: Student,
     modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(R.dimen.padding_small))
-    ) {
-        StudentIcon(student.imageResourceId)
-        StudentInformation(student.name, student.age)
+)
+ {
+    Card(modifier = modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            StudentIcon(student.imageResourceId)
+            StudentInformation(student.name, student.age)
+        }
     }
 }
 @Composable
  fun StudentApp() {
-     LazyColumn {
-         items(students) {
-             StudentItem(student = it)
-         }
-     }
- }
+    Scaffold { it ->
+        LazyColumn(contentPadding = it) {
+            items(students) {
+                StudentItem(
+                    student = it,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
+        }
+    }
+}
+
 
 @Preview
 @Composable
