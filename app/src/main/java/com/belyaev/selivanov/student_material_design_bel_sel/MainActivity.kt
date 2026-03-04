befdfb101ab2e6218a81773d17578a097c2434a6
s0m1k111 <spavel1207@gmail.com>
2026-03-04 16:36:40 +0300

    Step 10: Top App Bar added

--- app/src/main/java/com/belyaev/selivanov/student_material_design_bel_sel/MainActivity.kt
+++ app/src/main/java/com/belyaev/selivanov/student_material_design_bel_sel/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.draw.clip
@@ -32,6 +33,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.belyaev.selivanov.student_material_design_bel_sel.data.students
 import kotlinx.serialization.descriptors.SerialKind
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -101,7 +106,11 @@ fun StudentItem(
 }
 @Composable
  fun StudentApp() {
-    Scaffold { it ->
+    Scaffold (
+        topBar =  {
+            StudentTopAppBar()
+        }
+    ){ it ->
         LazyColumn(contentPadding = it) {
             items(students) {
                 StudentItem(
@@ -113,7 +122,33 @@ fun StudentItem(
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StudentTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_student_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier
+    )
+}
 
 @Preview
 @Composable
